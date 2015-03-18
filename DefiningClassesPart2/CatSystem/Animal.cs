c72dcbf21a6b36cb77e3c01f16f1611988e92a6c
aliv59git <vilavaln@gmail.com>
2015-03-19 00:25:23 +0200

    new v

---
+++ DefiningClassesPart2/CatSystem/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CatSystem
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ DefiningClassesPart2/CatSystem/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatSystem
+{
+    public class Dog : Animal
+    {
+        public string SeyBow()
+        {
+            return "Bow!";
+        }
+    }
+}
