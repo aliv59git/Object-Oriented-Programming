bb6da02b48527d8719690be6f7491e9c396fb91f
aliv59git <vilavaln@gmail.com>
2015-03-19 16:36:13 +0200

    new ver

---
+++ Home2DefiningClasses2/01.Structure/Distance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Structure
+{
+    public static class Distance
+    {
+        public static double DistanceBetweenTwoPoints(Point3D p1, Point3D p2)
+        {
+            return Math.Sqrt((p1.XCoord - p2.XCoord) * (p1.XCoord - p2.XCoord) + (p1.YCoord - p2.YCoord) * (p1.YCoord - p2.YCoord) + (p1.ZCoord - p2.ZCoord) * (p1.ZCoord - p2.ZCoord));
+        }
+    }
+}
