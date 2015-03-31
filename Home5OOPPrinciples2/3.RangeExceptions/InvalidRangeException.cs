b182ff3ef1955042e12f342fa308b1e48a50ef30
aliv59git <vilavaln@gmail.com>
2015-03-31 21:03:47 +0300

    new variant

--- Home5OOPPrinciples2/3.RangeExceptions/InvalidRangeException.cs
+++ Home5OOPPrinciples2/3.RangeExceptions/InvalidRangeException.cs
@@ -5,10 +5,45 @@ using System.Linq;
 
 namespace _3.RangeExceptions
 {
-    public class InvalidRangeException<T> : ArgumentException
+    public class InvalidRangeException<T> : ApplicationException
+        where T: IComparable<T>
     {
-        public int start;
-        public int end;
+        public T start;
+        public T end;
+
+        public InvalidRangeException(string msg, T start, T end, Exception ex)
+            : base(String.Format("{0} must be in range [{1}; {2}]", msg, start, end), ex)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+        
+
+        public InvalidRangeException(string msg, T start, T end)
+            :this(msg, start, end, null)
+        {
+        }
+
+        public InvalidRangeException(T start, T end)
+            : this(null, start, end, null)
+        {
+        }
+        public InvalidRangeException()
+        {
+        }
+
+        public T Start
+        {
+            get { return this.start; }
+            set { this.start = value; }
+        }
+
+        public T End
+        {
+            get { return this.end; }
+            set { this.end = value; }
+        }
+
 
     }
 }
--- Home5OOPPrinciples2/3.RangeExceptions/StartRangeExceptions.cs
+++ Home5OOPPrinciples2/3.RangeExceptions/StartRangeExceptions.cs
@@ -8,6 +8,24 @@ namespace _3.RangeExceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                throw new InvalidRangeException<int>("Invalid numbers! The numbers ", 1, 100);
+            }
+            catch (InvalidRangeException<int> ex)
+            {
+                Console.WriteLine("Numbers: {0}", ex.Message);
+            }
+            Console.WriteLine();
+            try
+            {
+                throw new InvalidRangeException<DateTime>("Date ", new DateTime(1980, 1, 1), DateTime.Now);
+            }
+            catch (InvalidRangeException<DateTime> ex)
+            {
+                Console.WriteLine("Invalid date! {0}", ex.Message);
+            }
+
         }
     }
 }
