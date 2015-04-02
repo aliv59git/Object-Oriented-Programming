c9bde932c35939120b389331d79f9389f54fa4d0
aliv59 <vilavaln@gmail.com>
2015-04-02 23:53:47 +0300

    var

---
+++ ExamPreparationNiki/FurnitureManufacturer/Models/Furniture.cs
@@ -0,0 +1,71 @@
+﻿using FurnitureManufacturer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureManufacturer.Models
+{
+    public abstract class Furniture :IFurniture
+    {
+
+public string Model
+{
+	get { throw new NotImplementedException(); }
+}
+
+public string Material
+{
+	get { throw new NotImplementedException(); }
+}
+
+public decimal Price
+{
+	  get 
+	{ 
+		throw new NotImplementedException(); 
+	}
+	  set 
+	{ 
+		throw new NotImplementedException(); 
+	}
+}
+
+public decimal Height
+{
+	get { throw new NotImplementedException(); }
+}
+}
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+
+                }
+            }
+        }
+
+        public string Material
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public decimal Height
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
