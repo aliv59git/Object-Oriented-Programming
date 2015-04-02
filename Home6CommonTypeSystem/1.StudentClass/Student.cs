f10d832f001d5acf3359a7625b0a4bc5c633b017
aliv59git <vilavaln@gmail.com>
2015-04-02 16:27:47 +0300

    new

--- Home6CommonTypeSystem/1.StudentClass/Student.cs
+++ Home6CommonTypeSystem/1.StudentClass/Student.cs
@@ -4,7 +4,25 @@ namespace _1.StudentClass
 {
     public class Student
     {
-        public Specialty specialty;
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private int? sSNumber;
+        private string permanentAddress;
+        private string email;
+        private int? course;
+        private University uinversity;
+        private Specialty speialty;
+        private Faculty faculty;
+
+        public Student(string firstName, string middleName = null, string lastName, int? sSNumber=null, string permanentAddress = null, string email = null, int? course = null, University university, Faculty faculty, Specialty specialty)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+            //this.SSNumber = sSNumber;
+            this.PermanentAddress = permanentAddress;
+        }
 
         public string FirstName { get; private set; }
         public string MiddleName { get; private set; }
@@ -14,11 +32,74 @@ namespace _1.StudentClass
         public string MobilePhone { get; set; }
         public string Email { get; set; }
         public int Course { get; set; }
-        public string Speciality
+        public Specialty Specialty
         {
-            get { return Specialty.specialty; }
-            set { this.specialty = value; }
+            get { return speialty; }
+            set { speialty = value; }
         }
+        public University University
+        {
+            get { return uinversity; }
+            set { uinversity = value; }
+        }
+        public Faculty Faculty
+        {
+            get { return faculty; }
+            set { faculty = value; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var objAsStudent = obj as Student;
+            if (!Object.Equals(objAsStudent.FirstName, this.FirstName))
+            {
+                return false;
+            }
+            if (!Object.Equals(objAsStudent.MiddleName, this.MiddleName))
+            {
+                return false;
+            }
+            if (!Object.Equals(objAsStudent.LastName, this.LastName))
+            {
+                return false;
+            }
+            if (objAsStudent.SSNumber!=this.SSNumber)
+            {
+                return false;
+            }
+            if (!Object.Equals(objAsStudent.PermanentAddress, this.PermanentAddress))
+            {
+                return false;
+            }
+            if (!Object.Equals(objAsStudent.MobilePhone, this.MobilePhone))
+            {
+                return false;
+            }
+            if (!Object.Equals(objAsStudent.Email, this.Email))
+            {
+                return false;
+            }
+            if (objAsStudent.Course != this.Course)
+            {
+                return false;
+            }
+            if (objAsStudent.University.GetHashCode() != this.University.GetHashCode())
+            {
+                return false;
+            } 
+            if (objAsStudent.Faculty.GetHashCode() != this.Faculty.GetHashCode())
+            {
+                return false;
+            }
+            if (objAsStudent.Specialty.GetHashCode() != this.Specialty.GetHashCode())
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+        
 
     }
 }
